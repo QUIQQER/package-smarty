d96066d0190f2a073117e13a9f1e5cf030d9502f
Henning Leutz <leutz@pcsg.de>
2013-04-24 15:13:08 +0200

    1.0.3; fix: standard project fix

--- Quiqqer/Engine/plugins_qui/function.url.php
+++ Quiqqer/Engine/plugins_qui/function.url.php
@@ -22,116 +22,116 @@
  */
 function smarty_function_url($params, $smarty)
 {
-	$url  = '';
+    $url  = '';
     $Site = false;
 
     try
     {
-        if (isset($params['project']))
+        if ( isset( $params['project'] ) )
         {
-            if (isset($params['lang']))
+            if ( isset( $params['lang'] ) )
             {
-                $_Project = QUI::getProject(
+                $_Project = \Projects_Manager::getProject(
                     $params['project'],
                     $params['lang']
                 );
             } else
             {
-                $_Project = QUI::getProject(
+                $_Project = \Projects_Manager::getProject(
                     $params['project'],
-                    QUI::getLocale()->getCurrent()
+                    \QUI::getLocale()->getCurrent()
                 );
             }
 
         } else
         {
-            $_Project = QUI::get();
+            $_Project = \Projects_Manager::get();
 
-            if (isset($params['lang']))
+            if ( isset( $params['lang'] ) )
             {
-                $_Project = QUI::getProject(
+                $_Project = \Projects_Manager::getProject(
                     $_Project->getAttribute('name'),
                     $params['lang']
                 );
             }
         }
-    } catch (QException $e)
+    } catch ( \QException $e )
     {
-        System_Log::writeException($e);
+        \System_Log::writeException( $e );
         return '';
     }
 
-    if (isset($params['site']))
-	{
-		$Site = $params['site'];
-	} elseif (isset($params['id']))
-	{
-		try
-		{
-			$Site = $_Project->get((int)$params['id']);
-		} catch (QException $e)
-		{
-			System_Log::writeException($e);
-			return '';
-		}
-	}
-
-	unset($params['site']);
-	unset($params['id']);
-	unset($params['lang']);
-
-	$assign = false;
-	$host   = false;
-
-	if (isset($params['assign']))
-	{
-	    $assign = $params['assign'];
-	    unset($params['assign']);
-	}
-
-    if (isset($params['host']))
+    if ( isset( $params['site'] ) )
     {
-		$host = true;
-		unset($params['host']);
-    }
-
-	if ($Site && $Site->getId())
-	{
-    	if (isset($params['rewrited']) && $params['rewrited'])
-    	{
-    	    unset($params['rewrited']);
-
-    	    $url = $Site->getUrl($params, true);
-    	} else
-    	{
-    		$url = $Site->getUrl($params);
-    	}
-    }
-
-    if ($host) {
-		$url = HOST . $url;
-    }
-
-    if (isset($params['relative']))
+        $Site = $params['site'];
+    } elseif ( isset( $params['id'] ) )
     {
-    	$url    = split('/', $url);
-    	$folder = split('/', $_SERVER['REQUEST_URI']);
-    	$last   = end($url);
-
-    	if (strpos($_SERVER['REQUEST_URI'], $last))
-    	{
-			$url = end($url);
-    	} else
-    	{
-			$url = str_replace('.html','', end($folder)) .'/'. end($url);
-    	}
+        try
+        {
+            $Site = $_Project->get((int)$params['id']);
+        } catch ( \QException $e )
+        {
+            \System_Log::writeException($e);
+            return '';
+        }
     }
 
-    if (!$assign) {
-		return $url;
-	}
+    unset( $params['site'] );
+    unset( $params['id'] );
+    unset( $params['lang'] );
 
-	$smarty->assign($assign, $url);
+    $assign = false;
+    $host   = false;
+
+    if ( isset( $params['assign'] ) )
+    {
+        $assign = $params['assign'];
+        unset( $params['assign'] );
+    }
+
+    if ( isset( $params['host'] ) )
+    {
+        $host = true;
+        unset( $params['host'] );
+    }
+
+    if ( $Site && $Site->getId() )
+    {
+        if ( isset( $params['rewrited'] ) && $params['rewrited'] )
+        {
+            unset( $params['rewrited'] );
+
+            $url = $Site->getUrl( $params, true );
+        } else
+        {
+            $url = $Site->getUrl( $params );
+        }
+    }
+
+    if ( $host ) {
+        $url = HOST . $url;
+    }
+
+    if ( isset( $params['relative'] ) )
+    {
+        $url    = split( '/', $url );
+        $folder = split( '/', $_SERVER['REQUEST_URI'] );
+        $last   = end( $url );
+
+        if ( strpos( $_SERVER['REQUEST_URI'], $last ) )
+        {
+            $url = end( $url );
+        } else
+        {
+            $url = str_replace( '.html','', end( $folder ) ) .'/'. end( $url );
+        }
+    }
+
+    if ( !$assign ) {
+        return $url;
+    }
+
+    $smarty->assign( $assign, $url );
 }
 
 ?>
