df3f8583cc3d57750579ac36d27241bdc905ba85
Henning Leutz <leutz@pcsg.de>
2015-03-06 12:52:03 +0100

    {locale_date}

---
+++ Quiqqer/Engine/plugins_qui/function.locale_date.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Global date format
+ *
+ * @param Array $params
+ * @param Smarty $Smarty
+ * @return String
+ */
+function smarty_function_locale_date($params, $Smarty)
+{
+    if ( !isset( $params['timestamp'] ) && !isset( $params['date'] ) ) {
+        return '';
+    }
+
+    $timestamp = false;
+
+    if ( isset( $params['date'] ) ) {
+        $timestamp = strtotime( $params['date'] );
+    }
+
+    if ( isset( $params['timestamp'] ) ) {
+        $timestamp = (int)$params['timestamp'];
+    }
+
+    if ( !$timestamp ) {
+        return '';
+    }
+
+
+    $format = false;
+
+    if ( isset( $params['format'] ) ) {
+        $format = $params['format'];
+    }
+
+    $result = QUI::getLocale()->formatDate( $timestamp, $format );
+
+    if ( !isset( $params['assign'] ) ) {
+        return $result;
+    }
+
+    $Smarty->assign( $params['assign'], $result );
+}
