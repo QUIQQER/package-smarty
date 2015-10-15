d07fce01c63ebb2c0f4d40f4d6290b110f346b08
Henning Leutz <leutz@pcsg.de>
2015-10-15 08:38:17 +0200

    neue url_linked funktion; macht es einfacher sprachverknüpfungen in templates zu setzen

---
+++ Quiqqer/Engine/plugins_qui/function.url_linked.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Smarty {url_linked Site= lang=en} function plugin
+ *
+ * Type:     function<br>
+ * Name:     url_linked<br>
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ *
+ * @param array $params -> GET params = _get__*
+ * @param \Smarty $smarty
+ *
+ * @return string
+ */
+function smarty_function_url_linked($params, $smarty)
+{
+    if (!isset($params['Site'])) {
+        return '';
+    }
+
+    if (!isset($params['lang'])) {
+        return '';
+    }
+
+    $wantedLang = $params['lang'];
+
+    /* @var $Site QUI\Projects\Site */
+    $Site    = $params['Site'];
+    $Project = $Site->getProject();
+
+    if ($Project->getLang() == $wantedLang) {
+        return $Site->getUrlRewritten();
+    }
+
+    try {
+        $LangProject = QUI::getProjectManager()->getProject(
+            $Project->getName(),
+            $wantedLang
+        );
+
+    } catch (QUI\Exception $Exception) {
+        return '';
+    }
+
+
+    try {
+
+        $langId = $Site->getId($params['lang']);
+        $LangSide = $LangProject->get($langId);
+
+        return $LangSide->getUrlRewritten();
+
+    } catch (QUI\Exception $Exception) {
+
+    }
+
+    try
+    {
+        return $LangProject->firstChild()->getUrlRewritten();
+
+    } catch (QUI\Exception $Exception) {
+
+    }
+
+    return '';
+}
